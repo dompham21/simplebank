b06e168bdac5ad5771825efa72adcf63123d2fd5
bac.pd <63381043+bac.pd@users.noreply.github.com>
2024-03-18 00:13:43 +0700

    make it simple

--- Makefile
+++ Makefile
@@ -1,33 +1,28 @@
-DB_URL=postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable
+postgres:
+	docker run --name postgres12 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:12-alpine
+
+mysql:
+	docker run --name mysql8 -p 3306:3306  -e MYSQL_ROOT_PASSWORD=secret -d mysql:8
 
 createdb:
-	docker exec -it postgres createdb --username=root --owner=root simple_bank
+	docker exec -it postgres12 createdb --username=root --owner=root simple_bank
 
 dropdb:
-	docker exec -it postgres dropdb simple_bank
+	docker exec -it postgres12 dropdb simple_bank
 
-postgres:
-	docker run --name postgres --network bank-network -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:12-alpine
-	
 migrateup:
-	migrate -path db/migration -database "$(DB_URL)" -verbose up
-
-migrateup1:
-	migrate -path db/migration -database "$(DB_URL)" -verbose up 1
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose up
 
 migratedown:
-	migrate -path db/migration -database "$(DB_URL)" -verbose down
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose down
 
-migratedown1:
-	migrate -path db/migration -database "$(DB_URL)" -verbose down 
-
-sqlc: 
+sqlc:
 	sqlc generate
 
 test:
-	go test -v -cover -short ./...
+	go test -v -cover ./...
 
 server:
 	go run main.go
 
-.PHONY: postgres createdb dropdb  migrateup migratedown sqlc test 
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server
--- api/server.go
+++ api/server.go
@@ -1,56 +1,27 @@
 package api
 
 import (
-	"fmt"
-
 	db "github.com/dompham21/simplebank/db/sqlc"
-	"github.com/dompham21/simplebank/token"
-	"github.com/dompham21/simplebank/util"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
 )
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
-	router     *gin.Engine
+	store  *db.Store
+	router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and set up routing.
-func NewServer(config util.Config, store db.Store) (*Server, error) {
-	// tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create token maker: %w", err)
-	}
-
-	server := &Server{
-		config: config,
-		store:  store,
-		// tokenMaker: tokenMaker,
-	}
-
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("currency", validCurrency)
-	}
-
-	server.setupRouter()
-	return server, nil
-}
-
-func (server *Server) setupRouter() {
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
 	router := gin.Default()
 
-	router.POST("/users", server.createUser)
-	router.POST("/users/login", server.loginUser)
-
-	authRoutes.POST("/accounts", server.createAccount)
-	authRoutes.GET("/accounts/:id", server.getAccount)
-	authRoutes.GET("/accounts", server.listAccounts)
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 
 	server.router = router
+	return server
 }
 
 // Start runs the HTTP server on a specific address.
